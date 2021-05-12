ef1c3614a8aeefc0d4824fe04c358dc5bf0e4a9d
Ivan <79006128+ivan-gi@users.noreply.github.com>
2021-05-12 21:09:09 +0300

    Add files via upload

---
+++ cats_test_2.py
@@ -0,0 +1,8 @@
+from cats_1 import Cat
+
+cat_1 = Cat("Baron", "boy", 2)
+cat_2 = Cat("Sam", "boy", 2)
+
+print(cat_1.get_name(), cat_1.get_gender(), cat_1.get_age())
+print(cat_2.get_name(), cat_2.get_gender(), cat_2.get_age())
+
